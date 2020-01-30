512f18129761def726fb86a3e3f736d938c2eb13
chrisbg <archangelbg@abv.bg>
2020-01-30 23:39:16 +0200

    update backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use = (bodyParser.json());
+app.use = (bodyParser.urlencoded({ extended: false }));
+
+
 
 app.use((req, res,next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -12,6 +17,7 @@ app.use((req, res,next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
+  const posts = req.body;
   console.log();
 });
 
