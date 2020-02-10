5dfed59d3400c6d2b72dfd88b187371b284e8161
chrisbg <archangelbg@abv.bg>
2020-02-11 00:33:11 +0200

    added check-auth middleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import {
   MatInputModule,
   MatCardModule,
@@ -22,6 +22,7 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 
 @NgModule({
@@ -50,7 +51,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatPaginatorModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,13 @@ import { AuthData } from "./auth-data.model";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) { }
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string) {
     const AuthData: AuthData = { email: email, password: password };
@@ -18,9 +24,10 @@ export class AuthService {
 
   login(email: string, password: string) {
     const AuthData: AuthData = { email: email, password: password };
-    this.http.post("http://localhost:3000/api/user/login", AuthData)
+    this.http.post<{token: string}>("http://localhost:3000/api/user/login", AuthData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 
