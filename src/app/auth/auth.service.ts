5648dc49b22f4e6b1ff02d82b4b2bf336863659d
chrisbg69 <archangelbg@abv.bg>
2020-02-11 17:59:07 +0200

    added routeGard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { AuthData } from "./auth-data.model";
+
 
 
 @Injectable({ providedIn: "root" })
@@ -11,7 +13,7 @@ export class AuthService {
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getToken() {
     return this.token;
@@ -43,6 +45,7 @@ export class AuthService {
         if (token) {
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
       });
   }
@@ -51,6 +54,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 
 }
