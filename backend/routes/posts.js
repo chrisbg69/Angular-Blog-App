803dd94f60c71bd9c3bde68075de9acec66ae9e7
chrisbg69 <archangelbg@abv.bg>
2020-02-12 17:46:12 +0200

    added authorization method

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -69,9 +69,10 @@ router.put(
       _id: req.body.id,
       title: req.body.title,
       content: req.body.content,
-      imagePath: imagePath
+      imagePath: imagePath,
+      creator: req.userData.userId
     });
-    console.log(post);
+
     Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
       if (result.nModified > 0) {
         res.status(200).json({ message: "Update successful!" });
