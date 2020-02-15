dd1b24efb07fc39083739330a593cb97caa55edb
chrisbg <archangelbg@abv.bg>
2020-02-15 19:18:19 +0200

    update error handling

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,29 +6,26 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-
-
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(result => {
-          res.status(201).json({
-            message: 'User created',
-            result: result
-          });
-        })
-        .catch(err => {
-          res.status(500).json({
-            error: err
-          });
-        });
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
     });
-
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Invalid authentication credentials!"
+        });
+      });
+  });
 });
 
 router.post("/login", (req, res, next) => {
@@ -62,7 +59,7 @@ router.post("/login", (req, res, next) => {
     })
     .catch(err => {
       return res.status(401).json({
-        message: "Auth failed"
+        message: "Invalid authentication credentials!"
       });
     });
 });
