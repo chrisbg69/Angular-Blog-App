fa11773c6fbc675b7dbddbc14971363ef64ffdc3
chrisbg <archangelbg@abv.bg>
2020-02-16 02:49:21 +0200

    change some notify messages

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -22,7 +22,7 @@ router.post("/signup", (req, res, next) => {
       })
       .catch(err => {
         res.status(500).json({
-          message: "Invalid authentication credentials!"
+          message: "Email must be unique!"
         });
       });
   });
@@ -34,7 +34,7 @@ router.post("/login", (req, res, next) => {
     .then(user => {
       if (!user) {
         return res.status(401).json({
-          message: "Auth failed"
+          message: "Auth failed, no such user!"
         });
       }
       fetchedUser = user;
@@ -43,7 +43,7 @@ router.post("/login", (req, res, next) => {
     .then(result => {
       if (!result) {
         return res.status(401).json({
-          message: "Auth failed"
+          message: "Auth failed, invalid password!"
         });
       }
       const token = jwt.sign(
